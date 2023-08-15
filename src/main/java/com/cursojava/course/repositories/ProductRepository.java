d6b4623d4aef2a7b223466bf2a68df88a2b15240
lucastandy <lucastitandy@gmail.com>
2023-08-15 20:20:28 -0300

    Product

---
+++ src/main/java/com/cursojava/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.cursojava.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursojava.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+
+}
